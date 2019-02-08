77db6d6c73916469305f0310441730c1ff2b9acf
Dmitry Boytsov <boytsov.dm@gmail.com>
2019-02-09 02:14:07 +0300

    Add back button to Forgot password page

--- assets/js/containers/PasswordForgot.js
+++ assets/js/containers/PasswordForgot.js
@@ -5,7 +5,8 @@ import Footer from '../components/Footer';
 import {handlePasswordForgot} from '../actions/user';
 
 const PasswordForgot = props => (
-    <div className="jd-page-footer">
+    <div className="jd-page-header-footer">
+        <div onClick={props.history.goBack} className="jd-back jd-mt-3 jd-ml-5">&#8249;</div>
         <div className="jd-center-container">
             <PasswordForgotForm onSubmit={props.handlePasswordForgot}/>
         </div>
